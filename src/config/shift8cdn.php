affa2ed2a83843d6100877a78fbc1bf1225b02d8
Kevin <admin@stardothosting.com>
2020-03-14 14:05:32 -0400

    Updated comment for config file

--- src/config/shift8cdn.php
+++ src/config/shift8cdn.php
@@ -4,11 +4,11 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Application Shift8 CDN domains
+    | Shift8 CDN domain for rewriting
     |--------------------------------------------------------------------------
     |
-    | Specify different domains for your assets.
-    |
+    | Specify the Shift8 CDN domain for your assets. 
+    | Supported file extensions : ico, css, js, gif, jpeg, jpg, png, svg, woff, woff2, ttf, eot, map
     */
     'Shift8CDN' => [
             "replace-me.wpcdn.shift8cdn.com" => "css|js|eot|woff|ttf|jpg|jpeg|png|gif|svg",
