9a07482d653d88af53bddaeaffc5acba1621fdb8
Kevin <admin@stardothosting.com>
2020-03-14 11:38:03 -0400

    first commit

---
+++ config/shift8cdn.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Shift8 CDN domains
+    |--------------------------------------------------------------------------
+    |
+    | Specify different domains for your assets.
+    |
+    */
+    'Shift8CDN' => [
+            "replace-me.wpcdn.shift8cdn.com" => "css|js|eot|woff|ttf|jpg|jpeg|png|gif|svg",
+    ]
+
+];
+
